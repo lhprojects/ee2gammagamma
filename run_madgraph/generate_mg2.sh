1246c56e6573438def0d8bfef659cb1bf7dc00a6
Harry <lihawl@126.com>
2021-10-11 15:00:48 +0800

    more detailed data

---
+++ run_madgraph/generate_mg2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+work_dir='/home/liangh/data/ee2gammagamma/PROC_sm_1'
+beams=(25 30 35 40 45 45.6 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 130 135  140)
+#beams=(25 30)
+
+for beam in ${beams[@]}
+do
+
+~/software/MG5_aMC_v2_6_7/bin/mg5_aMC << EOF
+launch ${work_dir}
+done
+set ebeam ${beam}
+set nevents 100000
+done
+EOF
+done
